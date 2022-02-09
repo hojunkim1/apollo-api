04b441a15fcd6a2b65864cf843ce9db586c92633
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2022-02-09 11:36:55 +0900

    use query in react

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,42 @@
+import { gql, useQuery } from "@apollo/client";
+import styles from "../styles/Home.module.css";
+
+const GET_MOVIES = gql`
+  query {
+    movies {
+      id
+      title
+      medium_cover_image
+    }
+  }
+`;
+
 const Home = () => {
-  return <div>Home!</div>;
+  const { loading, error, data } = useQuery(GET_MOVIES);
+
+  if (loading)
+    return (
+      <div className={styles.wrapper}>
+        <h1 className={styles.message}>Loading...</h1>
+      </div>
+    );
+  if (error)
+    return (
+      <div className={styles.wrapper}>
+        <h1 className={styles.message}>Error :(</h1>
+      </div>
+    );
+  if (data)
+    return (
+      <div className={styles.moviesWrapper}>
+        {data.movies.map((movie) => (
+          <div key={movie.id} className={styles.movie}>
+            <img src={movie.medium_cover_image} alt={movie.title} />
+            <h3>{movie.title}</h3>
+          </div>
+        ))}
+      </div>
+    );
 };
 
 export default Home;
